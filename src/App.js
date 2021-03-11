de43a458a762829815042ace36d544d013c39272
Mila Decker <milacdecker@gmail.com>
2021-03-11 12:26:29 -0500

    updated tests

--- src/App.js
+++ src/App.js
@@ -32,7 +32,9 @@ function App() {
                 currentCategory={currentCategory}
             ></Nav>
             <main>
-                <Gallery></Gallery>
+                <Gallery
+                    currentCategory={currentCategory}
+                ></Gallery>
                 <About></About>
             </main>
         </div>
