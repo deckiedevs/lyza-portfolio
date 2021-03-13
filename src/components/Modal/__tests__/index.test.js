bf354783ea9d60a3ce92d3b38059d6e6167c9555
Mila Decker <milacdecker@gmail.com>
2021-03-12 22:17:47 -0500

    added tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+const mockToggleModal = jest.fn();
+
+const currentPhoto = { 
+    name: 'Park bench',
+    category: 'landscape',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 4
+}
+
+
+afterEach(cleanup);
+
+describe('Modal component', () => {
+    // renders Modal
+    it('renders', () => {
+        render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal} />);
+    });
+
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal} />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
+
+describe('Click Event', () => {
+    it('calls onClose handler', () => {
+        // Arrange: Render
+        const { getByText } = render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal} />);
+
+        // Act: Simulate
+        fireEvent.click(getByText('Close this modal'));
+
+        // Assert: Expectation
+        expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    });
+});
